1645e847d842c695e0bfba89f34d54aa325ac28c
Gianluca <gianluca@dicapua.me>
2024-07-08 17:17:18 +0200

    fix test

--- src/test/java/com/cino86/DateServices/DateControllerIntegrationTest.java
+++ src/test/java/com/cino86/DateServices/DateControllerIntegrationTest.java
@@ -10,14 +10,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 
 @SpringBootTest(classes = DateServicesApplication.class)
 @AutoConfigureMockMvc
@@ -38,10 +41,37 @@ public class DateControllerIntegrationTest {
         dataInput.setTipoDelta("lavorativi");
         dataInput.setCalendariFestivi(List.of("Milano"));
 
-        mockMvc.perform(post("/api/date/calcolaData")
+        String expectedJson = objectMapper.writeValueAsString(new DataResponse(LocalDate.of(2024, 7, 19)));
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/date/calcolaData")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(dataInput)))
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+                mockMvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(new DataResponse(LocalDate.of(2024, 7, 19)))));
+                .andExpect(content().json(expectedJson));
+    }
+
+    @Test
+    public void testCalcolaDataPassata() throws Exception {
+        DataInput dataInput = new DataInput();
+        dataInput.setDataPartenza(LocalDate.of(2024, 7, 7));
+        dataInput.setDeltaGiorni(-10);
+        dataInput.setTipoDelta("lavorativi");
+        dataInput.setCalendariFestivi(List.of("Milano"));
+
+        String expectedJson = objectMapper.writeValueAsString(new DataResponse(LocalDate.of(2024, 6, 24)));
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/date/calcolaData")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dataInput)))
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+                mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
     }
 }
